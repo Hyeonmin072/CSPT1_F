4bb78715cccb1bee1130b733987d7cf00ca71eba
김현민 <gusals072@gmail.com>
2025-01-07 16:25:49 +0900

    이미지 데이터 임시 바인딩

--- frontend/src/components/layout/IconGrid.jsx
+++ frontend/src/components/layout/IconGrid.jsx
@@ -1,21 +1,31 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
+// 아이콘 이미지들 import
+import icon1 from "../../assets/icons/icon1.png"; // 경로는 실제 위치에 맞게 수정
+import icon2 from "../../assets/icons/icon2.png";
+import icon3 from "../../assets/icons/icon3.png";
+import icon4 from "../../assets/icons/icon4.png";
+import icon5 from "../../assets/icons/icon5.png";
+import icon6 from "../../assets/icons/icon6.png";
+import icon7 from "../../assets/icons/icon7.png";
+import icon8 from "../../assets/icons/icon8.png";
+
 export default function IconGrid() {
-  const colorClasses = [
-    "bg-indigo-100",
-    "bg-indigo-200",
-    "bg-indigo-300",
-    "bg-indigo-400",
-    "bg-blue-400",
-    "bg-blue-300",
-    "bg-blue-200",
-    "bg-blue-100",
+  // 아이콘 배열 생성
+  const icons = [
+    { id: 1, icon: icon1, color: "bg-indigo-100" },
+    { id: 2, icon: icon2, color: "bg-indigo-200" },
+    { id: 3, icon: icon3, color: "bg-indigo-300" },
+    { id: 4, icon: icon4, color: "bg-indigo-400" },
+    { id: 5, icon: icon5, color: "bg-blue-400" },
+    { id: 6, icon: icon6, color: "bg-blue-300" },
+    { id: 7, icon: icon7, color: "bg-blue-200" },
+    { id: 8, icon: icon8, color: "bg-blue-100" },
   ];
 
   return (
     <div className="w-full max-w-4xl mx-auto mt-24 px-4 relative">
-      {/* 왼쪽 그라데이션 오버레이 */}
       <div
         className="absolute left-0 top-0 h-full w-24 z-10 pointer-events-none"
         style={{
@@ -31,8 +41,8 @@ export default function IconGrid() {
         loop={true}
         className="h-[150px]"
       >
-        {colorClasses.map((color, index) => (
-          <SwiperSlide key={index}>
+        {icons.map(({ id, icon, color }) => (
+          <SwiperSlide key={id}>
             <div
               className={`h-[100px] rounded-xl transition-all duration-300 ${color}
                           shadow-soft
@@ -41,13 +51,16 @@ export default function IconGrid() {
                           hover:scale-90
                         `}
             >
-              <span className="text-black-700 font-medium">{index + 1}</span>
+              <img
+                src={icon}
+                alt={`icon ${id}`}
+                className="w-12 h-12 object-contain" // 아이콘 크기 조절
+              />
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
 
-      {/* 오른쪽 그라데이션 오버레이 */}
       <div
         className="absolute right-0 top-0 h-full w-24 z-10 pointer-events-none"
         style={{
--- frontend/src/components/layout/MainLogo.jsx
+++ frontend/src/components/layout/MainLogo.jsx
@@ -1,8 +1,14 @@
+import logo from "../../assets/logo/logo.png";
+
 export default function MainLogo() {
   return (
     <div className="flex justify-center">
       <div className="w-[250px] h-[250px] bg-pink-300 rounded-lg shadow-lg mt-10 flex justify-center">
-        Myong
+        <img
+          src={logo}
+          alt="logo"
+          className="w-full h-full object-contain p-4" // padding을 주어 여백 확보
+        />
       </div>
     </div>
   );
