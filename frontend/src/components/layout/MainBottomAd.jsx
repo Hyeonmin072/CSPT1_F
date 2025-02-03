f59c20f2a339d7f2c9594024c66df4a89fef0fda
김현민 <gusals072@gmail.com>
2025-02-03 17:13:24 +0900

    슬라이더 상세 주석 추가

--- frontend/src/components/layout/MainBottomAd.jsx
+++ frontend/src/components/layout/MainBottomAd.jsx
@@ -22,16 +22,26 @@ export default function MainBottomAd({ className, height }) {
     // 광고 이미지 슬라이더
     <div className={`w-300px sm:mt-2 ${className}`}>
       <Swiper
+        // 스와이퍼 모델 import autoplay는 자동 재생 모드
         modules={[Autoplay]}
+        // 슬라이드 간 간격 설정 0으로 간격 X
         spaceBetween={0}
+        // 한번에 보여 줄 슬라이드 개수 설정, 광고는 하나 씩 넘어가야 하므로 1로 설정
         slidesPerView={1}
+        // 무한 루프 설정 true
         loop={true}
+        // 오토플레이 상세 설정
         autoplay={{
+          // 딜레이 5000
           delay: 5000,
-          disableOnInteraction: false,
+          // 사용자가 슬라이더와 상호 작용하면 슬라이드 멈춤
+          disableOnInteraction: true,
         }}
       >
+        {/* 각 배열 요소를 순회하며 슬라이드 생성 */}
         {adImages.map((ad) => (
+          // 스와이퍼에서 사용되는 각 슬라이드 컴포넌트
+          // Key는 React가 요소를 구분하기 위한 고유 식별자
           <SwiperSlide key={ad.id}>
             <div className="rounded-lg w-full h-full">
               <img
