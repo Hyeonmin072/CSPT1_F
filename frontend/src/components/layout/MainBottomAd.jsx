ba4d43bd97f70a436aeab750ad2452ecdcfaa9fe
김현민 <gusals072@gmail.com>
2025-02-03 17:58:49 +0900

    AD 영역 이벤트 핸들러추가 및 관련 주석 추가

--- frontend/src/components/layout/MainBottomAd.jsx
+++ frontend/src/components/layout/MainBottomAd.jsx
@@ -1,6 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
+import { useRef } from "react";
 
 // 이미지 import
 import ad1 from "../../assets/AD/ad1.png";
@@ -10,6 +11,27 @@ import ad4 from "../../assets/AD/ad4.png";
 
 //eslint-disable-next-line
 export default function MainBottomAd({ className, height }) {
+  const swiperRef = useRef(null); // Swiper 참조를 위한 ref 생성
+
+  // hover 이벤트 핸들러 2종
+
+  // 마우스가 영역에 들어왔을 때 실행
+  const handleMouseEnter = () => {
+    //swiperRef.current가 존재하고 autoplay 속성인지 확인
+    if (swiperRef.current && swiperRef.current.autoplay) {
+      // 맞다면 autoplay 정지지
+      swiperRef.current.autoplay.stop();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    //swiperRef.current가 존재하고 autoplay 속성인지 확인
+    if (swiperRef.current && swiperRef.current.autoplay) {
+      // 맞다면 autoplay 시작
+      swiperRef.current.autoplay.start();
+    }
+  };
+
   // 광고 이미지 데이터
   const adImages = [
     { id: 1, img: ad1, alt: "ad1" },
@@ -20,8 +42,19 @@ export default function MainBottomAd({ className, height }) {
 
   return (
     // 광고 이미지 슬라이더
-    <div className={`w-300px sm:mt-2 ${className}`}>
+    <div
+      className={`w-300px sm:mt-2 ${className}`}
+      // 마우스 이벤트 핸들러 2종 등록
+
+      // 마우스가 영역에 들어올 때
+      onMouseEnter={handleMouseEnter}
+      // 마우스가 영역을 벗어날 때
+      onMouseLeave={handleMouseLeave}
+    >
       <Swiper
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper; // Swiper 인스턴스 저장
+        }}
         // 스와이퍼 모델 import autoplay는 자동 재생 모드
         modules={[Autoplay]}
         // 슬라이드 간 간격 설정 0으로 간격 X
@@ -43,7 +76,7 @@ export default function MainBottomAd({ className, height }) {
           // 스와이퍼에서 사용되는 각 슬라이드 컴포넌트
           // Key는 React가 요소를 구분하기 위한 고유 식별자
           <SwiperSlide key={ad.id}>
-            <div className="rounded-lg w-full h-full">
+            <div className="rounded-lg w-full h-full hover:opacity-10">
               <img
                 src={ad.img}
                 alt={ad.alt}
