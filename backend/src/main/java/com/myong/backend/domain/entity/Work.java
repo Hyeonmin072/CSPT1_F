d831d59ec16a4ebb44c4e61256889a46ebf56338
김현민 <gusals072@gmail.com>
2025-02-04 15:23:31 +0900

    FE develop branch BE 관련 내용 삭제

--- backend/src/main/java/com/myong/backend/domain/entity/Work.java
+++
@@ -1,5 +0,0 @@
-package com.myong.backend.domain.entity;
-
-public enum Work {
-    FULLTIME, CONTACT //정규직, 계약직
-}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
-
 function App() {
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="rounded-lg bg-white p-8 shadow-lg">
-        <h1 className="text-2xl font-bold text-gray-800">
-          HAIRTALK TEST
-        </h1>
+        <h1 className="text-2xl font-bold text-gray-800">HAIRTALK TEST</h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
