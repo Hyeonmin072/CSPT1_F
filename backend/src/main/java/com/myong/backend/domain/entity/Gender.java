1da3ffacef7948d526cf8d480b6876bb4485655c
nanaling1137 <dlfwnd98@gmail.com>
2025-02-05 14:18:16 +0900

    헤어샵 상세정보 일부 수정, 뒤로 가기 추가

--- backend/src/main/java/com/myong/backend/domain/entity/Gender.java
+++
@@ -1,8 +0,0 @@
-package com.myong.backend.domain.entity;
-
-import jakarta.persistence.Enumerated;
-
-
-public enum Gender {
-    MALE, FEMALE // 남자, 여자
-}
--- backend/src/main/java/com/myong/backend/domain/entity/Status.java
+++
@@ -1,5 +0,0 @@
-package com.myong.backend.domain.entity;
-
-public enum Status {
-    NO, LATE, WORK, LEAVE, VACATION, REST  //    결근, 지각, 출근, 퇴근, 휴가, 휴직
-}
