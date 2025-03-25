a4917da921b37b6d3a9508cde7ae45c50344f759
김현민 <gusals072@gmail.com>
2025-03-25 15:40:17 +0900

    내 로컬 변경사항 저장

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,57 @@ import UserProfile from "./pages/profile/UserProfile";
 import UserProfileEdit from "./pages/profile/UserProfileEdit";
 import LoginAndRegisterPage from "./pages/loginandregister/LoginAndRegisterPage.jsx";
 import SubscriptDesignerPage from "./pages/designer/SubscriptDesignerPage.jsx";
-import DesignerSelectPage from "./pages/reservation/DesignerSelectPage.jsx"
-import CalendarSelectPage from "./pages/reservation/CalendarSelectPage.jsx"
-import MenuSelectPage from "./pages/reservation/MenuSelectPage.jsx"
+import DesignerSelectPage from "./pages/reservation/DesignerSelectPage.jsx";
+import CalendarSelectPage from "./pages/reservation/CalendarSelectPage.jsx";
+import MenuSelectPage from "./pages/reservation/MenuSelectPage.jsx";
 import ReviewsPage from "./pages/reviews/ReviewsPage.jsx";
 import PhotoReview from "./pages/reviews/PhotoReview.jsx";
 import ReservationCheckPage from "./pages/reservation/reservationcheck/ReservationCheckPage.jsx";
-
 import DesignerMainPage from "./pages/main/DesignerMainPage.jsx";
 import CurriculumVitaePage from "./pages/cv/CurriculumVitaePage.jsx";
 import SalesPage from "./pages/salesstatus/SalesPage.jsx";
+import SignIntegrational from "./pages/loginandregister/LoginAndRegisterPage.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
-  // 임시 역할 설정(customer, designer, business)
-  const userRole = 'designer';
+  // localStorage에서 userType을 가져와서 userRole로 변환
+  const [userRole, setUserRole] = useState(() => {
+    const userType = localStorage.getItem("userType");
+    // userType을 userRole로 변환 (SHOP -> business, USER -> customer, DESIGNER -> designer)
+    switch (userType) {
+      case "SHOP":
+        return "SHOP";
+      case "USER":
+        return "USER";
+      case "DESIGNER":
+        return "designer";
+      default:
+        return "USER"; // 기본값
+    }
+  });
+
+  // userType이 변경될 때마다 userRole 업데이트
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const userType = localStorage.getItem("userType");
+      switch (userType) {
+        case "SHOP":
+          setUserRole("SHOP");
+          break;
+        case "USER":
+          setUserRole("USER");
+          break;
+        case "DESIGNER":
+          setUserRole("DESIGNER");
+          break;
+        default:
+          setUserRole("USER");
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
 
   return (
     <Router>
@@ -30,36 +67,59 @@ function App() {
           <Routes>
             {/* 공통 */}
             <Route
-                path="/loginandregister"
-                element={<LoginAndRegisterPage />}
+              path="/loginandregister"
+              element={<LoginAndRegisterPage />}
             />
+            <Route path="/signintergrational" element={<SignIntegrational />} />
 
             {/* 고객 전용 */}
-            {userRole === 'customer' && (
-                <>
-                  <Route path="/" element={<MainPage />} />
-                  <Route path="/hairshop" element={<HairShopPage />} />
-                  <Route path="/detail" element={<HairShopDetailPage />} />
-                  <Route path="/designerpage" element={<DesignerPage />} />
-                  <Route path="/userprofile" element={<UserProfile />} />
-                  <Route path="/userprofileedit" element={<UserProfileEdit />} />
-                  <Route path="/designerselect" element={<DesignerSelectPage />} />
-                  <Route path="/calendarselect" element={<CalendarSelectPage />} />
-                  <Route path="/menuselect" element={<MenuSelectPage />} />
-                  <Route path="/reviews" element={<ReviewsPage />} />
-                  <Route path="/reviews/photo" element={<PhotoReview />} />
-                  <Route path="/reservationcheck" element={<ReservationCheckPage />} />
-                  <Route path="/subscriptdesigner" element={<SubscriptDesignerPage />} />
-                </>
+            {userRole === "USER" && (
+              <>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/hairshop" element={<HairShopPage />} />
+                <Route path="/detail" element={<HairShopDetailPage />} />
+                <Route path="/designerpage" element={<DesignerPage />} />
+                <Route path="/userprofile" element={<UserProfile />} />
+                <Route path="/userprofileedit" element={<UserProfileEdit />} />
+                <Route
+                  path="/designerselect"
+                  element={<DesignerSelectPage />}
+                />
+                <Route
+                  path="/calendarselect"
+                  element={<CalendarSelectPage />}
+                />
+                <Route path="/menuselect" element={<MenuSelectPage />} />
+                <Route path="/reviews" element={<ReviewsPage />} />
+                <Route path="/reviews/photo" element={<PhotoReview />} />
+                <Route
+                  path="/reservationcheck"
+                  element={<ReservationCheckPage />}
+                />
+                <Route
+                  path="/subscriptdesigner"
+                  element={<SubscriptDesignerPage />}
+                />
+              </>
+            )}
+
+            {/* 사장님 전용 */}
+            {userRole === "SHOP" && (
+              <>
+                <Route path="/" element={<HairShopPage />} />
+                <Route path="/hairshop" element={<HairShopPage />} />
+                <Route path="/detail" element={<HairShopDetailPage />} />
+                <Route path="/sales" element={<SalesPage />} />
+              </>
             )}
 
             {/* 디자이너 전용 */}
-            {userRole === 'designer' && (
-                <>
-                  <Route path="/" element={<DesignerMainPage />} />
-                  <Route path="/cv" element={<CurriculumVitaePage />} />
-                  <Route path="/sales" element={<SalesPage />} />
-                </>
+            {userRole === "designer" && (
+              <>
+                <Route path="/" element={<DesignerMainPage />} />
+                <Route path="/cv" element={<CurriculumVitaePage />} />
+                <Route path="/sales" element={<SalesPage />} />
+              </>
             )}
           </Routes>
         </main>
