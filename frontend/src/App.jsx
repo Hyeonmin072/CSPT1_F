972473a92c638abb58486f4ae9aa2ef720b78493
hyeongseopE <gudtjq363636@naver.com>
2025-05-27 17:27:58 +0900

    디자이너 채팅 페이지 및 채팅 윈도우 일부 로직 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 // 공통 컴포넌트
 import Footer from "./components/common/Footer";
 
-import ChattingPage from "./pages/chatting/ChattingPage.jsx";
+
 
 // 고객
 import MainPage from "./pages/main/MainPage.jsx";
@@ -30,6 +30,7 @@ import DesignerMatchPage from "./pages/designer/DesignerMatchPage";
 import ReservationConfirmPage from "./pages/reservation/ReservationConfirmPage.jsx";
 import ReservationLastCheckPage from "./pages/reservation/ReservationLastCheckPage.jsx";
 import DesignerInfoPage from "./pages/designer/DesignerInfoPage.jsx";
+import ChattingPage from "./pages/chatting/ChattingPage.jsx";
 
 // 디자이너
 import DesignerMainPage from "./pages/main/DesignerMainPage.jsx";
@@ -42,6 +43,7 @@ import CVCheck from "./pages/cv/CVCheckPage.jsx";
 import DesignerProfilePage from "./pages/profile/DesignerProfilePage.jsx";
 import DesignerProfileEditPage from "./pages/profile/DesignerProfileEditPage.jsx";
 import WeekNotice from "./components/DesingerAbout/main/notice/WeekNotice.jsx";
+import DesignerChattingPage from "./pages/chatting/DesignerChattingPage.jsx";
 
 // 사업자
 import BusinessMainPage from "./pages/main/BusinessMainPage.jsx";
@@ -56,6 +58,7 @@ import NoticesPage from "./pages/notices/NoticesPage.jsx";
 import RegisterNotice from "./pages/notices/RegisterNotice.jsx";
 import DetailNotice from "./pages/notices/DetailNotice.jsx";
 import EditNotice from "./pages/notices/EditNotice.jsx";
+
 
 
 function App() {
@@ -133,10 +136,6 @@ function App() {
           {/* 공통 라우트 */}
           <Route path="/social/signup" element={<SocialSignup />} />
           <Route
-            path="/chat"
-            element={<ChattingPage onLoginClick={openLoginModal} />}
-          />
-          <Route
             path="/hairshop"
             element={<HairShopPage onLoginClick={openLoginModal} />}
           />
@@ -199,6 +198,10 @@ function App() {
                 path="/reservationlastcheck"
                 element={<ReservationLastCheckPage />}
               />
+              <Route
+                path="/userchat"
+                element={<ChattingPage onLoginClick={openLoginModal} />}
+              />
             </>
           )}
 
@@ -256,6 +259,10 @@ function App() {
                   <DesignerProfileEditPage onLoginClick={openLoginModal} />
                 }
               />
+              <Route
+                path="/designerchat"
+                element={<DesignerChattingPage onLoginClick={openLoginModal} />}
+             />
             </>
           )}
 
