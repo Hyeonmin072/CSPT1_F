4783b1f99c3967522fee3b57b60a8e40a19e0497
rlawjggns <rlawjggns12@gmail.com>
2025-06-14 18:42:38 +0900

    배포용으로 axios 수정

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:1271", // 공통 주소
+    baseURL: import.meta.env.VITE_API_URL, // 공통 주소
     withCredentials: true,            // 쿠키 포함 (로그인 유지 등)
     headers: {
         "Content-Type": "application/json",
